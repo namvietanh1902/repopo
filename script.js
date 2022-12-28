92d74299f4ff0c0a62a74d097618a1e4f5ef8780
namvietanh1902 <85499555+namvietanh1902@users.noreply.github.com>
2022-12-28 16:02:03 +0700

    tab thu 2

--- script.js
+++ script.js
@@ -12,22 +12,20 @@ window.onscroll = ()=>{
 };
 let defaultsClick = document.querySelectorAll(".defaultClick");
 for (let i = 0; i < defaultsClick.length; i++) {
+  
   defaultsClick[i].click();
 }
 
-function openTab(evt, cityName) {
+function openTab(evt,contentClass, sectionName) {
 
-    const tabcontent = document.getElementsByClassName("tabcontent");
-    console.log(tabcontent);
+    const tabcontent = document.getElementsByClassName(contentClass+ " tabcontent");
     for (let i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    const tablinks = document.getElementsByClassName("tab");
+    const tablinks = document.getElementsByClassName(contentClass+" tab");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active","");
     }
-    document.getElementById(cityName).style.display = "flex";
+    document.getElementById(sectionName).style.display = "flex";
     evt.currentTarget.className += " active";
   }
