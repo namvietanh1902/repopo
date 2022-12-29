6e6bf44f473ea65f7e80fd62b5b9c8f67248ab8b
namvietanh1902 <85499555+namvietanh1902@users.noreply.github.com>
2022-12-29 16:33:58 +0700

    Bai tap js

--- Bai 7/app.js
+++ Bai 7/app.js
@@ -1,27 +1,37 @@
-function addUp(n){
-    return n*(n+1)/2;
-}
-function FizzBuzz(n){
-    let result = [];
-    for (let i =1;i<=n;i++){
-        if (i%15==0){
-            result.push("FizzBuzz");
-        }
-        else if (i%3==0){
-            result.push("Fizz");
-        }
-        else if (i%5==0){
-            result.push("Buzz");
-        }
-        else result.push(i);
-
+function biggestEvenNumber(input){
+    let arr = input.split(", ");
+    let evenArr = [];
+    for (let i = 0; i < arr.length; i++){ 
+        if (arr[i]%2==0) evenArr.push(parseInt(arr[i]));
     }
-    return result;
+    console.log(evenArr);
+    if (evenArr.length==0) return -1;
+    else{
+        let max = evenArr[0];
+        for (let i = 0; i < evenArr.length; i++){
+            if (evenArr[i]>max) max = evenArr[i];
+            
+        }
+        return max;
+    }
 }
+function fib( n)
+{
+    if (n <= 1) {
+        return n;
+    }
+ 
+    return fib(n - 1) + fib(n - 2);
+}
+const button1 = document.querySelector('.btn-1');
+const button2 = document.querySelector('.btn-2');
 
-const button = document.querySelector('.btn');
-button.addEventListener('click',function(){
-    const number = document.getElementById('UserInput');
-    console.log(FizzBuzz(number.value));
+button1.addEventListener('click',function(){
+    const number = document.getElementById('Bai1');
+    alert(biggestEvenNumber(number.value));
+})
+button2.addEventListener('click',function(){
+    const number = document.getElementById('Bai2');
+    alert(fib(number.value));
 })
 
